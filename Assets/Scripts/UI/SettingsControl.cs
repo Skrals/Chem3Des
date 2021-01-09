90354f232d4d6a12a2f6cc8c2af41198b399b330
Ivan <39524682+Skrals@users.noreply.github.com>
2021-01-09 13:16:45 +0300

    Settings interface functional

--- Assets/Scripts/UI/SettingsControl.cs
+++ Assets/Scripts/UI/SettingsControl.cs
@@ -6,17 +6,47 @@ using System;
 
 public class SettingsControl : MonoBehaviour
 {
-    public GameObject cameraSettings;
-    public GameObject MaxVal;
+    public GameObject cameraSettings;// настраиваемый объект
+    public GameObject MaxVal;// объекты вывода 
     public GameObject SpeedVal;
     public GameObject SenseVal;
+    public Slider sliderMax;// слайдеры ввода
+    public Slider sliderSpeed;
+    public Slider sliderSense;
 
-    [SerializeField] private float zoomMaxSet;
+    [SerializeField] private float zoomMaxSet;// вывод значений в интерфейс
     [SerializeField] private float speedSet;
     [SerializeField] private float senseSet;
+    private float tmpMax;// буфер минимальных значений
+    private float tmpSpeed;
+    private float tmpSense;
+    private void Start()// получаем минимальные значения
+    {
+        tmpMax = cameraSettings.GetComponent<CameraBuilder>().zoomMax;
+        tmpSpeed = cameraSettings.GetComponent<CameraBuilder>().zoom;
+        tmpSense = cameraSettings.GetComponent<CameraBuilder>().MouseSense;
 
+        sliderMax.minValue = tmpMax;
+        sliderSense.minValue = tmpSense;
+        sliderSpeed.minValue = tmpSpeed;
+
+    }
 
     void Update()
+    {
+        UpdateUIVals();
+        Settings();
+    }
+
+
+    private void Settings()// настройки через слайдеры
+    {
+        cameraSettings.GetComponent<CameraBuilder>().zoomMax = sliderMax.value;
+        cameraSettings.GetComponent<CameraBuilder>().zoom = sliderSpeed.value;
+        cameraSettings.GetComponent<CameraBuilder>().MouseSense = sliderSense.value;
+    }
+
+    private void UpdateUIVals()// обновление значений в интерфейсе
     {
         zoomMaxSet = cameraSettings.GetComponent<CameraBuilder>().zoomMax;
         speedSet = cameraSettings.GetComponent<CameraBuilder>().zoom;

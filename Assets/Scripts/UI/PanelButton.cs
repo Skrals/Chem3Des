c1799e909e416ac2dcc351d2750ef7aef725f25e
Ivan <39524682+Skrals@users.noreply.github.com>
2021-02-08 22:56:05 +0300

    buttons rework

--- Assets/Scripts/UI/PanelButton.cs
+++ Assets/Scripts/UI/PanelButton.cs
@@ -6,18 +6,16 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class PanelButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+public class PanelButton : MonoBehaviour, IPointerClickHandler
 {
     public GameObject panel;
-
-    [SerializeField] private Image image;
     [SerializeField] private string name;
     [SerializeField] private int sceneIndex;
     [SerializeField] private bool showed = false;
     private Vector2 sizeOffset;
     private RectTransform rectTransform;
 
-    void Start ()
+    void Start()
     {
         rectTransform = GetComponent<RectTransform>();
         sizeOffset = rectTransform.sizeDelta;
@@ -28,16 +26,16 @@ public class PanelButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
         name = eventData.pointerEnter.name;
         if (name == "Builder" && sceneIndex == 0) SceneManager.LoadScene(1);
         else if (name == "Main" && sceneIndex == 1) SceneManager.LoadScene(0);
-        else if (name == "Settings") 
-        { 
-            if (panel != null) 
+        else if (name == "Settings")
+        {
+            if (panel != null)
             {
                 bool isActive = panel.activeSelf;
                 panel.SetActive(!isActive);
             }
         }
         else if (name == "Exit") Application.Quit();
-        else if( name == "Elements")
+        else if (name == "Elements")
         {
             if (showed)
             {
@@ -50,29 +48,29 @@ public class PanelButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
         }
     }
 
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        if (tag == "Panel_button")
-        {
-            image = GetComponent<Image>();
-            image.color = Color.blue;
-            showed = true;
-        }
-    }
+    //public void OnPointerEnter(PointerEventData eventData)
+    //{
+    //    if (tag == "Panel_button")
+    //    {
+    //        image = GetComponent<Image>();
+    //        image.color = Color.blue;
+    //        showed = true;
+    //    }
+    //}
 
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        if (tag == "Panel_button")
-        {
-            image = GetComponent<Image>();
-            image.color = Color.white;
-            showed = false;
-        }
-    }
+    //public void OnPointerExit(PointerEventData eventData)
+    //{
+    //    if (tag == "Panel_button")
+    //    {
+    //        image = GetComponent<Image>();
+    //        image.color = Color.white;
+    //        showed = false;
+    //    }
+    //}
 
     void Update()
     {
-        if(showed)
+        if (showed)
         {
             Showed();
         }
@@ -80,27 +78,19 @@ public class PanelButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
         {
             Hide();
         }
-        if( sceneIndex == 0)
-        {
-            GameObject.Find("Main").GetComponent<Image>().color = Color.red;
-        }
-        else if( sceneIndex == 1)
-        {
-            GameObject.Find("Builder").GetComponent<Image>().color = Color.red;
-        }
+        //if (sceneIndex == 0)
+        //{
+        //    GameObject.Find("Main").GetComponent<Image>().color = Color.red;
+        //}
+        //else if (sceneIndex == 1)
+        //{
+        //    GameObject.Find("Builder").GetComponent<Image>().color = Color.red;
+        //}
     }
     private void Showed()
     {
         Vector2 pos = rectTransform.anchoredPosition;
-        if (tag == "Panel_button")
-        {
-            if (pos.y > -24)
-            {
-                float move = Time.deltaTime * 200;
-                rectTransform.anchoredPosition = new Vector3(pos.x, pos.y - move);
-            }
-        }
-        else if (tag == "Elements_panel")
+        if (tag == "Elements_panel")
         {
             if (pos.x < 140)
             {
@@ -112,15 +102,7 @@ public class PanelButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
     private void Hide()
     {
         Vector2 pos = rectTransform.anchoredPosition;
-        if (tag == "Panel_button")
-        {
-            if (pos.y > -sizeOffset.y && pos.y < 15)
-            {
-                float move = Time.deltaTime * 200;
-                rectTransform.anchoredPosition = new Vector3(pos.x, pos.y + move);
-            }
-        }
-        else if (tag == "Elements_panel")
+        if (tag == "Elements_panel")
         {
             if (pos.x > -120)
             {
---
+++ Assets/Scripts/UI/SpriteSwitch.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SpriteSwitch : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    public Sprite defoltSprite;
+    public Sprite switchOnSprite;
+    public Sprite usedSprite;
+
+    [SerializeField] string name;
+    void Start()
+    {
+        name = null;
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+         switchOn(switchOnSprite);
+       
+    }
+    
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (name != "Settings")
+        {
+            switchOn(defoltSprite);
+        }
+        else
+        {
+            switchOn(usedSprite);
+        }
+    }
+
+    void switchOn( Sprite sp)
+    {
+          this.GetComponent<Image>().sprite = sp;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (this.gameObject.name == "Settings")
+        {
+            if (name == null)
+            {
+                name = this.gameObject.name;
+            }
+            else
+            {
+                name = null;
+            }
+           switchOn(usedSprite);
+        }
+    }
+}

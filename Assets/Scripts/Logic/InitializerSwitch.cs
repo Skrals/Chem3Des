b95069d4c3a5fc2a6d630f36405af52633403822
Ivan <39524682+Skrals@users.noreply.github.com>
2021-03-04 21:23:47 +0300

    refactor initialization script

--- Assets/Scripts/Logic/InitializerSwitch.cs
+++ Assets/Scripts/Logic/InitializerSwitch.cs
@@ -4,44 +4,22 @@ using UnityEngine;
 
 public class InitializerSwitch : MonoBehaviour
 {
-      //Перенести сюда метод с параметром для приема объекта инициализации
-    public string currentName;
-    public ElementInitializer [] init;//удалить
+
+    [SerializeField] private GameObject element;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private Vector3 mousePositionVector;
+    public void Initializer(GameObject elem)
+    {
+        element = elem;
+    }
 
     private void Update()
     {
-        currentName = ElementInitializer.name;
-        switch (currentName)// переделать костыль
+        if (Input.GetKeyDown(KeyCode.Mouse0) && Input.GetKey(KeyCode.LeftShift) && element != null)
         {
-            case "H_button":
-                init[0].Initializer();
-                break;
-            case "C_button":
-                init[1].Initializer();
-                break;
-            case "O_button":
-                init[2].Initializer();
-                break;
-            case "N_button":
-                init[3].Initializer();
-                break;
-            case "S_button":
-                init[4].Initializer();
-                break;
-            case "F_button":
-                init[5].Initializer();
-                break;
-            case "CL_button":
-                init[6].Initializer();
-                break;
-            case "BR_button":
-                init[7].Initializer();
-                break;
-
+            offset = new Vector3(10f, 10f, 10f);
+            mousePositionVector = Camera.main.ScreenToWorldPoint(Input.mousePosition + offset);
+            Instantiate(element, (mousePositionVector), Quaternion.identity);
         }
-
-        
     }
-
-
 }
--- Assets/Scripts/UI/ElementInitializer.cs
+++ Assets/Scripts/UI/ElementInitializer.cs
@@ -6,32 +6,13 @@ using UnityEngine.EventSystems;
 public class ElementInitializer : MonoBehaviour, IPointerClickHandler
 {
     public GameObject element;
-    public Transform spawnPosition;// удалить
-    public GameObject initializer;// удалить
-
-    [SerializeField] private Vector3 offset;//перенести
-    [SerializeField] private Vector3 mousePositionVector;
-    public static string name;// удалить
-    public void OnPointerClick(PointerEventData eventData)// ??
+    public InitializerSwitch init;
+  
+    public void OnPointerClick(PointerEventData eventData)
     {
         name = eventData.pointerEnter.name;
+        init.Initializer(element);
         Debug.Log(name);
-    }
-
-    public void Initializer()//перенести в switch с параметром GameObject element
-    {
-        if (Input.GetKeyDown(KeyCode.Mouse0) && Input.GetKey(KeyCode.LeftShift) && element != null)
-        {
-            offset = new Vector3(10f, 10f, 10f);
-            mousePositionVector = Camera.main.ScreenToWorldPoint(Input.mousePosition + offset);
-            Instantiate(element, (mousePositionVector), Quaternion.identity);
-        }
-
-    }
-
-    void Start ()// удалить
-    {
-        initializer = GameObject.Find("ElementSwitcher");
     }
 
 }

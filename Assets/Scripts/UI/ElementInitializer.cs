f5d1d75c0f2761198f223aec0d4a166566d910c1
Ivan <39524682+Skrals@users.noreply.github.com>
2021-03-04 17:08:54 +0300

    Update ElementInitializer.cs

--- Assets/Scripts/UI/ElementInitializer.cs
+++ Assets/Scripts/UI/ElementInitializer.cs
@@ -6,19 +6,19 @@ using UnityEngine.EventSystems;
 public class ElementInitializer : MonoBehaviour, IPointerClickHandler
 {
     public GameObject element;
-    public Transform spawnPosition;
-    public GameObject initializer;
+    public Transform spawnPosition;// удалить
+    public GameObject initializer;// удалить
 
-    [SerializeField] private Vector3 offset;
+    [SerializeField] private Vector3 offset;//перенести
     [SerializeField] private Vector3 mousePositionVector;
-    public static string name;
-    public void OnPointerClick(PointerEventData eventData)
+    public static string name;// удалить
+    public void OnPointerClick(PointerEventData eventData)// ??
     {
         name = eventData.pointerEnter.name;
         Debug.Log(name);
     }
 
-    public void Initializer()
+    public void Initializer()//перенести в switch с параметром GameObject element
     {
         if (Input.GetKeyDown(KeyCode.Mouse0) && Input.GetKey(KeyCode.LeftShift) && element != null)
         {
@@ -29,7 +29,7 @@ public class ElementInitializer : MonoBehaviour, IPointerClickHandler
 
     }
 
-    void Start ()
+    void Start ()// удалить
     {
         initializer = GameObject.Find("ElementSwitcher");
     }
